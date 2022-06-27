e77fb04bd473f4d029f6e665950aba388da80209
nburgdorfer <nburgdorfer@gmail.com>
2022-06-27 14:35:18 -0400

    updates to scripts

--- scripts/test_on_dtu.sh
+++ scripts/test_on_dtu.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 ### parameters ###
-OUTPUT_PATH="/media/nate/Data/UCSNet/dtu/testing_results"
-SCENE_LIST="./dataloader/datalist/dtu/testing.txt"
-DATA_PATH="/media/nate/Data/UCSNet/dtu/testing/"
+OUTPUT_PATH="/media/nate/Data/UCSNet/dtu/testing_all_results"
+SCENE_LIST="./dataloader/datalist/dtu/testing_all.txt"
+DATA_PATH="/media/nate/Data/UCSNet/dtu/testing_all/"
 EXE_PATH="/home/nate/dev/research/fusibile/fusibile"
 EVAL=/media/nate/Data/Evaluation/dtu/
 EVAL_CODE_DIR=${EVAL}matlab_code/
@@ -17,26 +17,27 @@ CUDA_VISIBLE_DEVICES=0 python test.py --root_path $DATA_PATH --test_list $SCENE_
 
 
 ### Fusion ###
-SCANS=(1 4 9 10 11 12 13 15 23 24 29 32 33 34 48 49 62 75 77 110 114 118)
-
-for SCAN_NUM in ${SCANS[@]}
-do
-    DENSE_FOLDER="${OUTPUT_PATH}/scan${SCAN_NUM}"
-    CUDA_VISIBLE_DEVICES=0 python depthfusion.py --dense_folder $DENSE_FOLDER --fusibile_exe_path $EXE_PATH --prob_threshold 0.6 --disp_threshold 0.25 --num_consistent 3
-done
-
-# move merged point cloud to evaluation path
-python utils/collect_pointclouds.py --root_dir $OUTPUT_PATH --target_dir ${OUTPUT_PATH}/point_clouds --dataset "dtu"
-cp ${OUTPUT_PATH}/point_clouds/* ${EVAL_PC_DIR}
-
-
-## Evaluate the output point clouds
-# delete previous results if 'Results' directory is not empty
-if [ "$(ls -A $EVAL_RESULTS_DIR)" ]; then
-	rm -r $EVAL_RESULTS_DIR*
-fi
-
-USED_SETS="[${SCANS[@]}]"
-
-# run matlab evaluation on merged output point cloud
-matlab -nodisplay -nosplash -nodesktop -r "clear all; close all; format compact; arg_method='${PC_DIR_NAME}'; UsedSets=${USED_SETS}; run('${EVAL_CODE_DIR}BaseEvalMain_web.m'); clear all; close all; format compact; arg_method='${PC_DIR_NAME}'; UsedSets=${USED_SETS}; run('${EVAL_CODE_DIR}ComputeStat_web.m'); exit;" | tail -n +10
+#	#SCANS=({1..24} {28..53} {55..72} {74..77} {82..128})
+#	SCANS=(1 4 9 10 11 12 13 15 23 24 29 32 33 34 48 49 62 75 77 110 114 118)
+#	
+#	for SCAN_NUM in ${SCANS[@]}
+#	do
+#	    DENSE_FOLDER="${OUTPUT_PATH}/scan${SCAN_NUM}"
+#	    CUDA_VISIBLE_DEVICES=0 python depthfusion.py --dense_folder $DENSE_FOLDER --fusibile_exe_path $EXE_PATH --prob_threshold 0.6 --disp_threshold 0.25 --num_consistent 3
+#	done
+#	
+#	# move merged point cloud to evaluation path
+#	python utils/collect_pointclouds.py --root_dir $OUTPUT_PATH --target_dir ${OUTPUT_PATH}/point_clouds --dataset "dtu"
+#	cp ${OUTPUT_PATH}/point_clouds/* ${EVAL_PC_DIR}
+#	
+#	
+#	## Evaluate the output point clouds
+#	# delete previous results if 'Results' directory is not empty
+#	if [ "$(ls -A $EVAL_RESULTS_DIR)" ]; then
+#		rm -r $EVAL_RESULTS_DIR*
+#	fi
+#	
+#	USED_SETS="[${SCANS[@]}]"
+#	
+#	# run matlab evaluation on merged output point cloud
+#	matlab -nodisplay -nosplash -nodesktop -r "clear all; close all; format compact; arg_method='${PC_DIR_NAME}'; UsedSets=${USED_SETS}; run('${EVAL_CODE_DIR}BaseEvalMain_web.m'); clear all; close all; format compact; arg_method='${PC_DIR_NAME}'; UsedSets=${USED_SETS}; run('${EVAL_CODE_DIR}ComputeStat_web.m'); exit;" | tail -n +10
--- scripts/test_on_tanks.sh
+++ scripts/test_on_tanks.sh
@@ -9,15 +9,15 @@ CUDA_VISIBLE_DEVICES=0 python test.py --root_path $DATA_PATH --test_list $SCENE_
 
 
 ### Fusion ###
-EXE_PATH="/home/nate/dev/research/fusibile/fusibile"
-
-for SCENE in Barn Caterpillar Church Courthouse Ignatius Meetingroom Truck
-do
-	DENSE_FOLDER="${OUTPUT_PATH}/${SCENE}"
-	DISP=0.25
-	NUM_CONST=5
-	PROB_TH=0.6
-	CUDA_VISIBLE_DEVICES=0 python depthfusion.py --dense_folder $DENSE_FOLDER --fusibile_exe_path $EXE_PATH --prob_threshold $PROB_TH --disp_threshold $DISP --num_consistent $NUM_CONST
-done
-
-python utils/collect_pointclouds.py --root_dir $OUTPUT_PATH --target_dir ${OUTPUT_PATH}/point_clouds --dataset "tanks"
+#	EXE_PATH="/home/nate/dev/research/fusibile/fusibile"
+#	
+#	for SCENE in Barn Caterpillar Church Courthouse Ignatius Meetingroom Truck
+#	do
+#		DENSE_FOLDER="${OUTPUT_PATH}/${SCENE}"
+#		DISP=0.25
+#		NUM_CONST=5
+#		PROB_TH=0.6
+#		CUDA_VISIBLE_DEVICES=0 python depthfusion.py --dense_folder $DENSE_FOLDER --fusibile_exe_path $EXE_PATH --prob_threshold $PROB_TH --disp_threshold $DISP --num_consistent $NUM_CONST
+#	done
+#	
+#	python utils/collect_pointclouds.py --root_dir $OUTPUT_PATH --target_dir ${OUTPUT_PATH}/point_clouds --dataset "tanks"
